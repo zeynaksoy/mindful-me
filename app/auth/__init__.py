8e243883ba3c7db8408d67163d723e59fc2a7564
zeynaksoy <zeynaksoy05@gmail.com>
2026-05-24 19:35:28 +0300

    veritabanı modelleri oluşturuldu.

--- app/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-# Views will be imported here
-# from app.auth import views
--- app/main/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-main = Blueprint('main', __name__)
-
-# Views will be imported here
-# from app.main import views
