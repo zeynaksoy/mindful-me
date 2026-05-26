16b24b20416a143d485f6bbd84790984929a37e4
zeynaksoy <zeynaksoy05@gmail.com>
2026-05-26 20:13:26 +0300

    kullanıcı profili ve avatar yükleme yapısı eklendi
    - ProfileForm (avatar yükleme) forms.py içerisine tanımlandı.
    
    - Resim dosyaları için FileAllowed (jpg, png, vb.) doğrulama kuralları eklendi.

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import TextAreaField, SelectField, SubmitField, FloatField, IntegerField, StringField
 from wtforms.validators import DataRequired, Optional, NumberRange
 
@@ -15,3 +16,9 @@ class MoodEntryForm(FlaskForm):
     stress_level = IntegerField('Stres Seviyesi (1-10)', validators=[Optional(), NumberRange(min=1, max=10)])
     activities = StringField('Aktiviteler (virgülle ayırın)', validators=[Optional()])
     submit = SubmitField('Kaydet')
+
+class ProfileForm(FlaskForm):
+    avatar = FileField('Avatar Yükle', validators=[
+        FileAllowed(['jpg', 'jpeg', 'png', 'gif', 'webp'], 'Sadece resim dosyaları yüklenebilir!')
+    ])
+    submit = SubmitField('Profili Güncelle')
